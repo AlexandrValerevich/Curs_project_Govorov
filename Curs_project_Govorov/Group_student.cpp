bd78fc541418d4d5685d1ed5a6852fc73fe3e4a8
Alexander Nesterovich <sasha.nest92@gmail.com>
2021-05-15 21:06:42 +0300

    complited Group

--- Curs_project_Govorov/Group_student.cpp
+++ Curs_project_Govorov/Group_student.cpp
@@ -1,2 +1,276 @@
 #include "Group_student.h"
+#include "dbQuery.h"
+
+
+/*----------------------------------СОБЫТИЕ ЗАГРУЗКИ ФОРМЫ--------------------------------------------*/
+
+System::Void CursprojectGovorov::MyFormGroup::MyFormGroup_Load(System::Object^ sender, System::EventArgs^ e)
+{
+    dataGridViewGroup->Rows->Clear();
+
+    String^ connectionString = StringConnection(); //строка подключения
+    OleDbConnection^ dbConnection = gcnew OleDbConnection(connectionString);
+
+    dbConnection->Open();//открываем соединение
+
+    String^ SELECT = "group_id, speciality_name, group_name";
+    String^ FROM = "speciality INNER JOIN group_t ON group_t.speciality_id = speciality.speciality_id";
+
+
+    OleDbDataReader^ dbReader = SelectRow(dbConnection, SELECT, FROM); //вызов предыдущей команды
+
+    while (dbReader->Read())
+    {
+        dataGridViewGroup->Rows->Add(
+            dbReader[0],
+            dbReader[1],
+            dbReader[2]);//заносим строки в таблицу
+    }
+
+    //закрываем соединения
+    dbReader->Close();
+
+    if (domainUpDownSpeciality->Items->Count == 1) {
+        SELECT = "speciality_name";
+        FROM = "speciality";
+
+        dbReader = SelectRow(dbConnection, SELECT, FROM);
+
+        while (dbReader->Read())
+            domainUpDownSpeciality->Items->Add(dbReader[0]);
+
+        //закрываем соединения
+        dbReader->Close();
+    }
+    domainUpDownSpeciality->Text = domainUpDownSpeciality->Items[0]->ToString();
+
+    //Закрываем соединение
+    dbConnection->Close();
+    return System::Void();
+}
+
+/*-----------------------------------КНОПКИ ДОБАВИТЬ, УДАЛИТЬ, ИЗМЕНИТЬ-----------------------------------*/
+
+System::Void CursprojectGovorov::MyFormGroup::buttonAdd_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    String^ group_name = "'" + textBoxGroup->Text->ToString() + "'";
+
+    if (String::IsNullOrEmpty(group_name->Trim('\''))) {
+        MessageBox::Show("Введите название специальности!", "Внимание!");
+        textBoxGroup->Focus();
+        return;
+    }
+
+    String^ speciality_name = "'" + domainUpDownSpeciality->Text->ToString() + "'";
+
+    if (speciality_name == "'Все'") {
+        MessageBox::Show("Выберите специальность!", "Внимание!");
+        domainUpDownSpeciality->Focus();
+        return;
+    }
+
+    String^ speciality_id;
+
+    String^ connectionString = StringConnection(); //строка подключения
+    OleDbConnection^ dbConnection = gcnew OleDbConnection(connectionString);
+
+    dbConnection->Open();//открываем соединение
+
+    String^ SELECT = "speciality_id";
+    String^ FROM = "speciality";
+    String^ WHERE = "speciality_name LIKE " + speciality_name;
+    String^ TABLE;
+    String^ COLUMN;
+    String^ VALUES;
+
+    speciality_id = SelectID(dbConnection, SELECT, FROM, WHERE);
+
+    TABLE = "group_t";
+    COLUMN = "speciality_id, group_name";
+    VALUES = speciality_id + ", " + group_name;
+
+    if (!InsertRow(dbConnection, TABLE, COLUMN, VALUES))
+        MessageBox::Show("Ошибка в момент добавления!", "Внимание!");
+    else
+        MessageBox::Show("Запись успешно добавлена!");
+
+    MyFormGroup_Load(nullptr, nullptr);
+
+    //закрываем соединения
+    dbConnection->Close();
+    return System::Void();
+}
+
+System::Void CursprojectGovorov::MyFormGroup::buttonChange_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    if (dataGridViewGroup->SelectedCells->Count == 0) {
+        MessageBox::Show("Выберите строку которую нужно изменить!", "Внимание!");
+        return;
+    }
+
+    int index = dataGridViewGroup->SelectedCells[0]->RowIndex;
+
+    if (dataGridViewGroup->Rows->Count - 1 == index) //проверяем чтобы это не юыла последняя строка
+    {
+        MessageBox::Show("Это не строка!", "Внимание!"); //очистка полей ввода
+        return;
+    }
+
+    String^ group_id = dataGridViewGroup->Rows[index]->Cells[0]->Value->ToString();
+    String^ group_name = "'" + textBoxGroup->Text->ToString() + "'";
+
+    if (String::IsNullOrEmpty(group_name->Trim('\''))) {
+        MessageBox::Show("Введите имя кафедры!", "Внимание!");
+        textBoxGroup->Focus();
+        return;
+    }
+
+    String^ speciality_name = "'" + domainUpDownSpeciality->Text->ToString() + "'";
+
+    if (speciality_name == "'Все'") {
+        MessageBox::Show("Выберите факультет!", "Внимание!");
+        domainUpDownSpeciality->Focus();
+        return;
+    }
+
+    String^ speciality_id;
+
+    String^ connectionString = StringConnection(); //строка подключения
+    OleDbConnection^ dbConnection = gcnew OleDbConnection(connectionString);
+
+    dbConnection->Open();//открываем соединение
+
+    String^ SELECT = "speciality_id";
+    String^ FROM = "speciality";
+    String^ WHERE = "speciality_name LIKE " + speciality_name;
+    String^ TABLE;
+    String^ SET;
+
+    speciality_id = SelectID(dbConnection, SELECT, FROM, WHERE);
+
+    TABLE = "group_t";
+    SET = "group_name = " + group_name + ", speciality_id = " + speciality_id;
+    WHERE = "group_id = " + group_id;
+
+    if (!UpdateRow(dbConnection, TABLE, SET, WHERE))
+        MessageBox::Show("Ошибка в момент обновления!", "Внимание!");
+    else
+        MessageBox::Show("Запись успешно обновлена!");
+
+    MyFormGroup_Load(nullptr, nullptr);
+
+    //закрываем соединения
+    dbConnection->Close();
+
+    return System::Void();
+}
+
+System::Void CursprojectGovorov::MyFormGroup::buttonDelete_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    if (dataGridViewGroup->SelectedCells->Count == 0) {
+        MessageBox::Show("Выберите строку которую нужно удалить!", "Внимание!");
+        return;
+    }
+
+    int index = dataGridViewGroup->SelectedCells[0]->RowIndex;
+
+    if (dataGridViewGroup->Rows->Count - 1 == index) //проверяем чтобы это не юыла последняя строка
+    {
+        MessageBox::Show("Это не строка!", "Внимание!");
+        return;
+    }
+
+    String^ group_id = dataGridViewGroup->Rows[index]->Cells[0]->Value->ToString();
+
+    String^ connectionString = StringConnection(); //строка подключения
+    OleDbConnection^ dbConnection = gcnew OleDbConnection(connectionString);
+
+    dbConnection->Open();//открываем соединение
+
+    String^ FROM = "group_t";
+    String^ WHERE = "group_id = " + group_id;
+
+
+    if (!DeleteRow(dbConnection, FROM, WHERE))
+        MessageBox::Show("Ошибка в момент удаления!", "Внимание!");
+    else
+        MessageBox::Show("Запись успешно удалена!");
+
+    MyFormGroup_Load(nullptr, nullptr);
+
+    //закрываем соединения
+    dbConnection->Close();
+    return System::Void();
+}
+
+/*------------------------------ЗАПОЛНЕНИЕ И ОЧИСТКА ТЕКСТБОКСОВ------------------------------------------------*/
+
+System::Void CursprojectGovorov::MyFormGroup::dataGridViewGroup_CellClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e)
+{
+    if (dataGridViewGroup->SelectedRows->Count != 0)
+        return;
+
+    if (dataGridViewGroup->SelectedCells->Count == 0)
+        return;
+
+    int index = dataGridViewGroup->SelectedCells[0]->RowIndex;
+
+    if (dataGridViewGroup->Rows->Count - 1 == index) //проверяем чтобы это не юыла последняя строка
+    {
+        ClearTextBox(); //очистка полей ввода
+        return;
+    }
+
+    textBoxGroup->Text = dataGridViewGroup->Rows[index]->Cells["Group"]->Value->ToString();
+    domainUpDownSpeciality->Text = dataGridViewGroup->Rows[index]->Cells["Speciality"]->Value->ToString();
+    return System::Void();
+}
+
+System::Void CursprojectGovorov::MyFormGroup::dataGridViewGroup_RowEnter(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e)
+{
+    if (dataGridViewGroup->SelectedRows->Count == 0)
+        return;
+
+    int index = dataGridViewGroup->SelectedRows[0]->Index; //берем индекс первой выбранной строки
+
+    if (dataGridViewGroup->Rows->Count - 1 == index) //проверяем чтобы это не юыла последняя строка
+    {
+        ClearTextBox(); //очистка полей ввода
+        return;
+    }
+
+    textBoxGroup->Text = dataGridViewGroup->Rows[index]->Cells["Group"]->Value->ToString();
+    domainUpDownSpeciality->Text = dataGridViewGroup->Rows[index]->Cells["Speciality"]->Value->ToString();
+    return System::Void();
+}
+
+System::Void CursprojectGovorov::MyFormGroup::domainUpDownSpeciality_SelectedItemChanged(System::Object^ sender, System::EventArgs^ e)
+{
+    if (dataGridViewGroup->SelectedRows->Count != 0)
+        return;
+
+    auto rows = dataGridViewGroup->Rows;
+    int count_row = rows->Count - 1;
+    String^ selecting_rows = domainUpDownSpeciality->Text;
+
+    for (int i = 0; i < count_row; i++)
+        rows[i]->Visible = true;
+
+    if (selecting_rows == "Все")
+        return;
+
+    for (int i = 0; i < count_row; i++)
+        if (rows[i]->Cells["Speciality"]->Value->ToString() != selecting_rows)
+            rows[i]->Visible = false;
+
+    return System::Void();
+}
+
+System::Void CursprojectGovorov::MyFormGroup::ClearTextBox()
+{
+    textBoxGroup->Text = nullptr;
+    domainUpDownSpeciality->Text = domainUpDownSpeciality->Items[0]->ToString();
+    return System::Void();
+}
+
 
--- Curs_project_Govorov/Group_student.h
+++ Curs_project_Govorov/Group_student.h
@@ -46,11 +46,14 @@ namespace CursprojectGovorov {
 			}
 
 		}
-	private: System::Windows::Forms::DataGridView^ dataGridViewStudent;
+	private: System::Windows::Forms::DataGridView^ dataGridViewGroup;
+	protected:
+
 	protected:
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Name_student;
 	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::TextBox^ textBoxFaculties;
+	private: System::Windows::Forms::TextBox^ textBoxGroup;
+
 	private: System::Windows::Forms::Button^ buttonChange;
 	private: System::Windows::Forms::Button^ buttonDelete;
 
@@ -59,10 +62,14 @@ namespace CursprojectGovorov {
 
 
 	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::DomainUpDown^ domainUpDownFaculties;
+	private: System::Windows::Forms::DomainUpDown^ domainUpDownSpeciality;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ id;
-	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Specialyti;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Speciality;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ Group;
+
+
+
+
 
 
 
@@ -81,33 +88,35 @@ namespace CursprojectGovorov {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->dataGridViewStudent = (gcnew System::Windows::Forms::DataGridView());
+			this->dataGridViewGroup = (gcnew System::Windows::Forms::DataGridView());
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->textBoxFaculties = (gcnew System::Windows::Forms::TextBox());
+			this->textBoxGroup = (gcnew System::Windows::Forms::TextBox());
 			this->buttonChange = (gcnew System::Windows::Forms::Button());
 			this->buttonDelete = (gcnew System::Windows::Forms::Button());
 			this->buttonAdd = (gcnew System::Windows::Forms::Button());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->domainUpDownFaculties = (gcnew System::Windows::Forms::DomainUpDown());
+			this->domainUpDownSpeciality = (gcnew System::Windows::Forms::DomainUpDown());
 			this->id = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			this->Specialyti = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->Speciality = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->Group = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridViewStudent))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridViewGroup))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// dataGridViewStudent
+			// dataGridViewGroup
 			// 
-			this->dataGridViewStudent->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
-			this->dataGridViewStudent->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridViewStudent->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(3) {
+			this->dataGridViewGroup->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
+			this->dataGridViewGroup->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridViewGroup->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(3) {
 				this->id,
-					this->Specialyti, this->Group
+					this->Speciality, this->Group
 			});
-			this->dataGridViewStudent->Location = System::Drawing::Point(12, 124);
-			this->dataGridViewStudent->Name = L"dataGridViewStudent";
-			this->dataGridViewStudent->ReadOnly = true;
-			this->dataGridViewStudent->Size = System::Drawing::Size(594, 344);
-			this->dataGridViewStudent->TabIndex = 0;
+			this->dataGridViewGroup->Location = System::Drawing::Point(12, 124);
+			this->dataGridViewGroup->Name = L"dataGridViewGroup";
+			this->dataGridViewGroup->ReadOnly = true;
+			this->dataGridViewGroup->Size = System::Drawing::Size(594, 344);
+			this->dataGridViewGroup->TabIndex = 0;
+			this->dataGridViewGroup->CellClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyFormGroup::dataGridViewGroup_CellClick);
+			this->dataGridViewGroup->RowEnter += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyFormGroup::dataGridViewGroup_RowEnter);
 			// 
 			// label1
 			// 
@@ -118,14 +127,14 @@ namespace CursprojectGovorov {
 			this->label1->TabIndex = 2;
 			this->label1->Text = L"Группа";
 			// 
-			// textBoxFaculties
+			// textBoxGroup
 			// 
-			this->textBoxFaculties->Font = (gcnew System::Drawing::Font(L"Montserrat", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->textBoxGroup->Font = (gcnew System::Drawing::Font(L"Montserrat", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBoxFaculties->Location = System::Drawing::Point(142, 67);
-			this->textBoxFaculties->Name = L"textBoxFaculties";
-			this->textBoxFaculties->Size = System::Drawing::Size(120, 26);
-			this->textBoxFaculties->TabIndex = 3;
+			this->textBoxGroup->Location = System::Drawing::Point(142, 67);
+			this->textBoxGroup->Name = L"textBoxGroup";
+			this->textBoxGroup->Size = System::Drawing::Size(120, 26);
+			this->textBoxGroup->TabIndex = 3;
 			// 
 			// buttonChange
 			// 
@@ -138,6 +147,7 @@ namespace CursprojectGovorov {
 			this->buttonChange->TabIndex = 7;
 			this->buttonChange->Text = L"Изменить";
 			this->buttonChange->UseVisualStyleBackColor = true;
+			this->buttonChange->Click += gcnew System::EventHandler(this, &MyFormGroup::buttonChange_Click);
 			// 
 			// buttonDelete
 			// 
@@ -150,6 +160,7 @@ namespace CursprojectGovorov {
 			this->buttonDelete->TabIndex = 6;
 			this->buttonDelete->Text = L"Удалить";
 			this->buttonDelete->UseVisualStyleBackColor = true;
+			this->buttonDelete->Click += gcnew System::EventHandler(this, &MyFormGroup::buttonDelete_Click);
 			// 
 			// buttonAdd
 			// 
@@ -162,6 +173,7 @@ namespace CursprojectGovorov {
 			this->buttonAdd->TabIndex = 5;
 			this->buttonAdd->Text = L"Добавить";
 			this->buttonAdd->UseVisualStyleBackColor = true;
+			this->buttonAdd->Click += gcnew System::EventHandler(this, &MyFormGroup::buttonAdd_Click);
 			// 
 			// label2
 			// 
@@ -172,15 +184,17 @@ namespace CursprojectGovorov {
 			this->label2->TabIndex = 8;
 			this->label2->Text = L"Специальность";
 			// 
-			// domainUpDownFaculties
+			// domainUpDownSpeciality
 			// 
-			this->domainUpDownFaculties->Location = System::Drawing::Point(142, 23);
-			this->domainUpDownFaculties->Name = L"domainUpDownFaculties";
-			this->domainUpDownFaculties->ReadOnly = true;
-			this->domainUpDownFaculties->Size = System::Drawing::Size(120, 23);
-			this->domainUpDownFaculties->Sorted = true;
-			this->domainUpDownFaculties->TabIndex = 9;
-			this->domainUpDownFaculties->Text = L"Все";
+			this->domainUpDownSpeciality->Items->Add(L"Все");
+			this->domainUpDownSpeciality->Location = System::Drawing::Point(142, 23);
+			this->domainUpDownSpeciality->Name = L"domainUpDownSpeciality";
+			this->domainUpDownSpeciality->ReadOnly = true;
+			this->domainUpDownSpeciality->Size = System::Drawing::Size(234, 23);
+			this->domainUpDownSpeciality->Sorted = true;
+			this->domainUpDownSpeciality->TabIndex = 9;
+			this->domainUpDownSpeciality->Text = L"Все";
+			this->domainUpDownSpeciality->SelectedItemChanged += gcnew System::EventHandler(this, &MyFormGroup::domainUpDownSpeciality_SelectedItemChanged);
 			// 
 			// id
 			// 
@@ -189,11 +203,11 @@ namespace CursprojectGovorov {
 			this->id->ReadOnly = true;
 			this->id->Visible = false;
 			// 
-			// Specialyti
+			// Speciality
 			// 
-			this->Specialyti->HeaderText = L"Специальность";
-			this->Specialyti->Name = L"Specialyti";
-			this->Specialyti->ReadOnly = true;
+			this->Speciality->HeaderText = L"Специальность";
+			this->Speciality->Name = L"Speciality";
+			this->Speciality->ReadOnly = true;
 			// 
 			// Group
 			// 
@@ -206,14 +220,14 @@ namespace CursprojectGovorov {
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 18);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(618, 480);
-			this->Controls->Add(this->domainUpDownFaculties);
+			this->Controls->Add(this->domainUpDownSpeciality);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->buttonChange);
 			this->Controls->Add(this->buttonDelete);
 			this->Controls->Add(this->buttonAdd);
-			this->Controls->Add(this->textBoxFaculties);
+			this->Controls->Add(this->textBoxGroup);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->dataGridViewStudent);
+			this->Controls->Add(this->dataGridViewGroup);
 			this->Font = (gcnew System::Drawing::Font(L"Montserrat", 9.749999F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::SizableToolWindow;
@@ -224,12 +238,21 @@ namespace CursprojectGovorov {
 			this->ShowIcon = false;
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Группы";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridViewStudent))->EndInit();
+			this->Load += gcnew System::EventHandler(this, &MyFormGroup::MyFormGroup_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridViewGroup))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
 
-	};
+private: System::Void MyFormGroup_Load(System::Object^ sender, System::EventArgs^ e);
+private: System::Void domainUpDownSpeciality_SelectedItemChanged(System::Object^ sender, System::EventArgs^ e);
+private: System::Void buttonAdd_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void buttonChange_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void buttonDelete_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void dataGridViewGroup_CellClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e);
+private: System::Void dataGridViewGroup_RowEnter(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e);
+private: System::Void ClearTextBox();
+};
 }
